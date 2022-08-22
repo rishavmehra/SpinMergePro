9821dbe3194be39a72ec96aaf718233186fe926f
Rishav Mehra <rishavmehra61@gmail.com>
2022-08-22 18:17:07 +0530

    learned: Formated Output

--- main.go
+++ main.go
@@ -7,7 +7,8 @@ func main() {
 	const conferenceTickets = 50
 	var remainingTickets = 50
 
-	fmt.Println("welcome to", conferenceName, "booking application")
-	fmt.Println("We have total of", conferenceTickets, "tickets and", remainingTickets, "are still available")
+	fmt.Printf("welcome to %v booking application\n", conferenceName)
+	fmt.Printf("We have total of %v tickets and %v are still available\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get you ticket here to attend")
+
 }
