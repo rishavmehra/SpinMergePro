8fdf9c14e9d565f7a07c0d44693781c332af212b
Rishav Mehra <rishavmehra61@gmail.com>
2022-08-22 17:36:23 +0530

    learned: variable & const

--- main.go
+++ main.go
@@ -1,4 +1,13 @@
-package main	
+package main
 
+import "fmt"
 
-Print("Hello GO!")
+func main() {
+	var conferenceName = "Go Conference"
+	const conferenceTickets = 50
+	var remainingTickets = 50
+
+	fmt.Println("welcome to", conferenceName, "booking application")
+	fmt.Println("We have total of", conferenceTickets, "tickets and", remainingTickets, "are still available")
+	fmt.Println("Get you ticket here to attend")
+}
