da7309223ff3f3953c303b033fad365d84ae6b33
rishavmehra <rishavmehra61@gmail.com>
2023-08-10 11:22:44 +0530

    Application running Script using Docker

--- applicationRunner.sh
+++ applicationRunner.sh
@@ -2,6 +2,6 @@
 
 cd backEnd/
 pwd
-docker build -t rmdocker/pdf-editor:$version .
+docker build -t rmdocker/spinMergePro:$version .
 docker run --rm -d -p 80:8080 rmdocker/pdf-editor:$version
 sleep 10
